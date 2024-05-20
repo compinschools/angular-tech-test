8af7088ce9a9d7f8cc4acd58fc7594a193a26489
Colin Smith <colinsmith@smithcolin.co.uk>
2024-05-20 18:33:39 +0100

    Functionally tested

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { environment } from '../../environment/environment';
-import axios from 'axios';
 import moment from 'moment';
 @Component({
   selector: 'app-company',
@@ -16,54 +15,64 @@ export class CompanyComponent {
   constructor(private httpClient: HttpClient) {}
 
   ParseDate(date: string) {
-    if(date.includes('\\'))
-      {
-        const s = date.split('\\')[0];
+    if (date.includes('\\')) {
+      const s = date.split('\\')[0];
 
-        const newDate=  new Date(`${s[2]}-${s[1]}-${s[0]}`);
-        return moment(newDate).format('D MMMM YYYY');
-      }
-    try
-    {
-     const newDate =  new Date(date);
+      const newDate = new Date(`${s[2]}-${s[1]}-${s[0]}`);
       return moment(newDate).format('D MMMM YYYY');
     }
-    catch(e)
-    {
+    try {
+      const newDate = new Date(date);
+      return moment(newDate).format('D MMMM YYYY');
+    } catch (e) {
       return undefined;
     }
   }
 
-  selectCompany(company: any){
+  selectCompany(company: any) {
     this.selectedCompany = company;
   }
 
-  selectListOfficers(company: any){
-    
-
-    const headers = new HttpHeaders({'Content-Type': 'application/json',
-    'X-Api-Key':environment.apiKey});
-    this.httpClient.get(`https://angular-exercise.trunarrative.cloud/TruProxyAPI/rest/Companies/v1/Officers?CompanyNumber=${company.company_number}`,{headers}).subscribe({
-      next: (data : any) => {
-        console.log(data);
-        this.selectedCompanyOfficers = data.items as any[];
-      },
-      error: (error) => console.error(error),
-    });
-    
+  selectListOfficers(company: any) {
+    if (!company) {
+      this.selectedCompanyOfficers = null;
+    } else {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'X-Api-Key': environment.apiKey,
+      });
+      this.httpClient
+        .get(
+          `https://angular-exercise.trunarrative.cloud/TruProxyAPI/rest/Companies/v1/Officers?CompanyNumber=${company.company_number}`,
+          { headers }
+        )
+        .subscribe({
+          next: (data: any) => {
+            console.log(data);
+            this.selectedCompanyOfficers = data.items as any[];
+          },
+          error: (error) => console.error(error),
+        });
+    }
   }
 
   fetchPosts() {
-   
-    const headers = new HttpHeaders({'Content-Type': 'application/json',
-    'X-Api-Key':environment.apiKey});
-    const param = this.searchText ? `?Query=${this.searchText}` : '';
-    this.httpClient.get(`https://angular-exercise.trunarrative.cloud/TruProxyAPI/rest/Companies/v1/Search${param}`,{headers}).subscribe({
-      next: (data : any) => {
-        console.log(data);
-        this.companies = data.items as any[];
-      },
-      error: (error) => console.error(error),
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-Api-Key': environment.apiKey,
     });
+    const param = this.searchText ? `?Query=${this.searchText}` : '';
+    this.httpClient
+      .get(
+        `https://angular-exercise.trunarrative.cloud/TruProxyAPI/rest/Companies/v1/Search${param}`,
+        { headers }
+      )
+      .subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.companies = data.items as any[];
+        },
+        error: (error) => console.error(error),
+      });
   }
 }
